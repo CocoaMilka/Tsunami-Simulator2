f195db5c2f0c44a86232519ce6768414fd56415e
Cocoa Milka <chocolatemilkcows@gmail.com>
2024-04-07 00:21:16 -0500

    fix meter functionality

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,45 +10,60 @@ public class Player : MonoBehaviour
     public RectTransform healthMeter;
     public RectTransform oxygenMeter;
 
-    public int health;
-    public int oxygen;
+    public float health = 1f; // Now in percentage (0 to 1)
+    public float oxygen = 1f; // Now in percentage (0 to 1)
+
+    private const float MaxMeterWidth = 800f; // Max length of the meter
 
     // Start is called before the first frame update
     void Start()
     {
         underwaterFilter.SetActive(false);
-
-        health = 800;
-        oxygen = 800;
     }
 
-    // Update is called once per frame
+    // FixedUpdate is used for consistent updates
     void FixedUpdate()
     {
         if (water.transform.position.y > gameObject.transform.position.y)
         {
             underwaterFilter.SetActive(true);
-
-            // decrease oxygen then update UI
-            oxygen--;
-            oxygenMeter.sizeDelta = new Vector2(oxygen, oxygenMeter.sizeDelta.y);
-
+            DecreaseOxygen();
         }
         else
         {
-            // Increase oxygen until max
-            if (oxygen < 800) 
-            { 
-                oxygen++; 
-                oxygenMeter.sizeDelta = new Vector2(oxygen, oxygenMeter.sizeDelta.y);
-            }
-
             underwaterFilter.SetActive(false);
+            IncreaseOxygen();
+        }
+
+        UpdateMeters();
+    }
+
+    void DecreaseOxygen()
+    {
+        // Decrease oxygen then update UI
+        if (oxygen > 0)
+        {
+            oxygen -= Time.fixedDeltaTime / 10; // 10 seconds to deplete oxygen
+        }
+        else if (health > 0) // If oxygen is depleted, start decreasing health
+        {
+            health -= Time.fixedDeltaTime / 20; // Health decreases half as fast as oxygen
+        }
+    }
+
+    void IncreaseOxygen()
+    {
+        // Increase oxygen until max and oxygen is not full
+        if (oxygen < 1f)
+        {
+            oxygen += Time.fixedDeltaTime / 30; // Replenish oxygen faster than it depletes
+            oxygen = Mathf.Min(oxygen, 1f); // Ensure oxygen does not exceed 100%
         }
     }
 
     void UpdateMeters()
     {
-
+        oxygenMeter.sizeDelta = new Vector2(oxygen * MaxMeterWidth, oxygenMeter.sizeDelta.y);
+        healthMeter.sizeDelta = new Vector2(health * MaxMeterWidth, healthMeter.sizeDelta.y);
     }
 }
